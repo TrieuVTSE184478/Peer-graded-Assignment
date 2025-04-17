c652d176b1b4c710a3bb95518eae6efc351c60ee
TrieuVTSE184478 <trieuvtse184478@fpt.edu.vn>
2025-04-17 20:07:44 +0700

    gallery.js

--- gallery.js
+++ gallery.js
@@ -1,1 +1,14 @@
+function upDate(previewPic) {
+    console.log("Alt text: " + previewPic.alt);
+    console.log("Image source: " + previewPic.src);
 
+    let imageDiv = document.getElementById("image");
+    imageDiv.innerHTML = previewPic.alt;
+    imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
+}
+
+function undo() {
+    let imageDiv = document.getElementById("image");
+    imageDiv.innerHTML = "Hover over an image below to display here.";
+    imageDiv.style.backgroundImage = "url('')";
+}
